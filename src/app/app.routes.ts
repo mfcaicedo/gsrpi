0d67f8ad7a8fd2d27ae73549f4fc4897579f131e
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-01-10 16:26:15 -0500

    feat se proteje las rutas con guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,27 @@
 import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './shared/layout/app.layout.component';
 import { WelcomeComponent } from './shared/components/welcome/welcome.component';
+import { accountGuard, authGuard } from './auth/guards';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/login/pages/login.component').then(m => m.LoginComponent),
+        canActivate: [accountGuard],
+    },
+    {
+        path: '',
+        component: AppLayoutComponent,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: '',
+                component: WelcomeComponent
+            },
+            {
+                path: 'user-management',
+                loadChildren: () => import('./user-management/user-management.routes').then(m => m.routes)
+            }
+        ]
 
-    path: '',
-    component: AppLayoutComponent,
-    children: [
-        {
-            path: '',
-            component: WelcomeComponent
-        }, 
-        {
-            path: 'user-management',
-            loadChildren: () => import('./user-management/user-management.routes').then(m => m.routes)
-        }
-    ]
-
-
-}];
+    }];
