4f0f7b43c9e37ad963ea1779d6f8497844d3a9ce
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-01-13 23:45:53 -0500

    feat: se modifica el authguard porque se debe esperar a que la sesion porque es una funcion asyncrona

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,9 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { map, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (!inject(AuthService).isAuthenticated()) {
+  if (!inject(AuthService).getSession().pipe(take(1), map(session => session !== null))) {
     inject(Router).navigate(['/login']);
     return false;
   }
