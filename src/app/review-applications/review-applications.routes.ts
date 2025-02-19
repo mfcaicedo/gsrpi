9b971db3a60947379a0a7caa5c9eebf139f7eb48
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-02-19 00:05:44 -0500

    feat: se agrega la ruta para la revision de la solicitud

--- src/app/review-applications/review-applications.routes.ts
+++ src/app/review-applications/review-applications.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { ListApplicationsReviewComponent } from "./UI/pages/list-applications-review/list-applications-review.component";
+import { ReviewApplicationComponent } from "./UI/pages/review-application/review-application.component";
 
 export const routes: Routes = [
     {
         path: 'listar-solicitudes-revision',
         component: ListApplicationsReviewComponent
+    },
+    {
+        path: 'revisar-solicitud/:id',
+        component: ReviewApplicationComponent
     }
 ];
