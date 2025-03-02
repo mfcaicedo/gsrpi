7975657a9b144318311d19581328b467cabf6a09
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-02 16:06:27 -0500

    feat: se agrega modelo para la metadata de los archivos

--- src/app/applications-recognition/domain/models/file.model.ts
+++ src/app/applications-recognition/domain/models/file.model.ts
@@ -2,14 +2,3 @@ export interface FileRequest {
     fileId: number;
     name: string;
 }
-
-export interface FileMetadataRequest {
-    name: string;
-    type: string; //PDF, DOCX, XLSX, JPG, PNG, MP4, MP3, etc
-    extension: string; //pdf, docx, xlsx, jpg, png, mp4, mp3, etc
-    path: string;
-    observations: string;
-    size: number;
-    state: boolean; //true: activo, false: inactivo
-    createAt: string;
-}
