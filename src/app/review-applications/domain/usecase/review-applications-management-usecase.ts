2f6852d5d298b0a2638fff37778b7185f88308f3
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-11 10:29:05 -0500

    feat: se implementa los servicios abstractos en el caso de uso para actualizar estado de una solicitud y guardar vailidacion de una solicitud

--- src/app/review-applications/domain/usecase/review-applications-management-usecase.ts
+++ src/app/review-applications/domain/usecase/review-applications-management-usecase.ts
@@ -1,5 +1,7 @@
 import { inject, Injectable } from "@angular/core";
 import { ReviewApplicationsGateway } from "../models/gateway/review-applications-gateway";
+import { ValidationApplication } from "../models/validation.model";
+import { ApplicationStatuses } from "../../../shared/utils/enums/review-applications.enum";
 
 @Injectable({
     providedIn: 'root'
@@ -14,6 +16,14 @@ export class ReviewApplicationsManagementUseCase {
 
     getFileById(fileId: number) {
         return this.reviewApplicationsGateway.getFileById(fileId);
+    }
+
+    saveValidationOfApplication(validation: Partial<ValidationApplication>) {
+        return this.reviewApplicationsGateway.saveValidationOfApplication(validation);
+    }
+
+    updateApplicationState(applicationId: number, state: ApplicationStatuses) {
+        return this.reviewApplicationsGateway.updateApplicationState(applicationId, state);
     }
 
 }
