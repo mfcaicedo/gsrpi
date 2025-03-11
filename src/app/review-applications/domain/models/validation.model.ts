838ac5186caa0bc61fd1c00837cf95ea268436c1
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-11 10:27:36 -0500

    feat: modelos para la validacion de una solicitud

---
+++ src/app/review-applications/domain/models/validation.model.ts
@@ -0,0 +1,17 @@
+import { Application } from "../../../shared/utils/models/applications-common.model";
+import { Person } from "../../../shared/utils/models/person.model";
+
+export interface ValidationApplication {
+    validationId: number;
+    validationState: string;
+    observations: string;
+    application: Partial<Application>;
+    validationType: Partial<ValidationType>;
+    person: Partial<Person>;
+}
+
+export interface ValidationType {
+    validationTypeId: number;
+    validationTypeName: string;
+    description: string;
+}
