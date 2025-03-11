0d0cb8e09283df9272ca4e31df561a12e9ec7f33
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-11 10:28:20 -0500

    feat: se agrega los metodos abstractos para guardar la validacion y actualizar el estado de un validacion

--- src/app/review-applications/domain/models/gateway/review-applications-gateway.ts
+++ src/app/review-applications/domain/models/gateway/review-applications-gateway.ts
@@ -1,7 +1,11 @@
 import { Observable } from "rxjs";
 import { GenericResponse } from "../../../../shared/utils/models/request-response.model";
+import { ValidationApplication } from "../validation.model";
+import { ApplicationStatuses } from "../../../../shared/utils/enums/review-applications.enum";
 
 export abstract class ReviewApplicationsGateway {
     abstract getAllApplicationsByFacultyId(facultyId: number): Observable<GenericResponse>;
     abstract getFileById(fileId: number): Observable<GenericResponse>;
+    abstract saveValidationOfApplication(validation: Partial<ValidationApplication>): Observable<GenericResponse>;
+    abstract updateApplicationState(applicationId: number, state: ApplicationStatuses): Observable<GenericResponse>;
 }
