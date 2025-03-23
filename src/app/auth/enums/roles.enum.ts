da3f7141c5e652e1d0724bf9551b10c9a5a00f4b
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-23 17:12:02 -0500

    feat: enum para los roles

---
+++ src/app/auth/enums/roles.enum.ts
@@ -0,0 +1,10 @@
+export enum RoleNames {
+    ADMIN = 'administrador',
+    ADMIN_CIARP = 'admin-ciarp',
+    TEACHER = 'docente',
+    CPD_SECRETARY = 'secretaria-cpd',
+    CPD_PRESIDENT = 'presidente-cpd',
+    CPD_MEMBER = 'integrante-cpd',
+    CIARP_SECRETARY = 'secretaria-ciarp',
+    CIARP_MEMBER = 'integrante-ciarp',
+}
