12ede1fa1d2bc34b2055d3b65d6c4d44a29e237e
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-23 17:11:19 -0500

    feat: se protege la rutas de una feature de soliictudes

--- src/app/applications-recognition/applications-recognition.routes.ts
+++ src/app/applications-recognition/applications-recognition.routes.ts
@@ -8,42 +8,62 @@ import { RegisterRelatedWorksComponent } from "./UI/pages/register-related-works
 import { TermsAndConditionsComponent } from "./UI/pages/terms-and-conditions/terms-and-conditions.component";
 import { UploadApplicationFilesComponent } from "./UI/pages/upload-application-files/upload-application-files.component";
 import { ListApplicationsComponent } from "./UI/pages/list-applications/list-applications.component";
+import { AuthorizedGuard } from "../auth/guards/authorized.guard";
+import { RoleNames } from "../auth/enums/roles.enum";
 
 export const routes: Routes = [
     {
         path: 'crear-solicitud',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: CreateApplicationComponent,
     },
     {
         path: 'registrar-solicitantes/step-2',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: RegisterApplicantsComponent
     },
     {
         path: 'registrar-datos-generales-produccion/step-3',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: RegisterGeneralProductionDataComponent
     },
     {
         path: 'registrar-tipo-produccion/step-4',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: RegisterProductionTypeComponent
     },
     {
         path: 'registrar-datos-especificos-produccion/step-5',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: RegisterSpecificProductionDataComponent
     },
     {
         path: 'registrar-trabajos-relacionados/step-6',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: RegisterRelatedWorksComponent
     }, 
     {
         path: 'terminos-condiciones/step-7',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: TermsAndConditionsComponent
     },
     {
         path: 'subir-archivos/step-8',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: UploadApplicationFilesComponent
     },
     {
         path: 'listar-solicitudes',
+        canActivate: [AuthorizedGuard],
+        data: { roles: [RoleNames.TEACHER] },
         component: ListApplicationsComponent
     }
 ];
