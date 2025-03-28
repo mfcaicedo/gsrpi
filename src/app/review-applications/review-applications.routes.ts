3e19221b1e1fc0f8d5dbeccf03aee1776b03fb05
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-03-28 16:49:13 -0500

    feat: se agrega ruta para ver detalle de una solicitud

--- src/app/review-applications/review-applications.routes.ts
+++ src/app/review-applications/review-applications.routes.ts
@@ -3,6 +3,7 @@ import { ListApplicationsReviewComponent } from "./UI/pages/list-applications-re
 import { ReviewApplicationComponent } from "./UI/pages/review-application/review-application.component";
 import { ListApplicationsReviewCpdMembersComponent } from "./UI/pages/list-applications-review-cpd-members/list-applications-review-cpd-members.component";
 import { RecommendPointsApplicationComponent } from "./UI/pages/recommend-points-application/recommend-points-application.component";
+import { ViewApplicationDetailComponent } from "./UI/pages/view-application-detail/view-application-detail.component";
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path: 'recomendar-puntos/:applicationId/:teacherApplicationId',
         component: RecommendPointsApplicationComponent
+    },
+    {
+        path: 'ver-detalle-solicitud/:id',
+        component: ViewApplicationDetailComponent
     }
 ];
