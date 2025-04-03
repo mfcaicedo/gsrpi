b4328214905b6eafd95d89b81a934714485f611f
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-04-03 15:42:01 -0500

    feat: se crea vista funcional para las solicitudes de reconocimiento para los miembros del ciarp

--- src/app/review-applications/UI/pages/ciarp/list-applications-review-ciarp-members/list-applications-review-ciarp-members.component.ts
+++ src/app/review-applications/UI/pages/ciarp/list-applications-review-ciarp-members/list-applications-review-ciarp-members.component.ts
@@ -1,11 +1,250 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Table, TableModule } from 'primeng/table';
+import { AuthService } from '../../../../../auth/auth.service';
+import { ApplicationStatuses } from '../../../../../shared/utils/enums/review-applications.enum';
+import { Application } from '../../../../../shared/utils/models/applications-common.model';
+import { KeyValueOption } from '../../../../../shared/utils/models/form-builder.model';
+import { CreateInitialConfigurationUsecase } from '../../../../../system-configuration/domain/usecase/create-initial-configuration-usecase';
+import { ReviewApplicationsManagementUseCase } from '../../../../domain/usecase/review-applications-management-usecase';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-list-applications-review-ciarp-members',
-  imports: [],
+  imports: [CommonModule, ButtonModule, RouterModule, IconFieldModule, InputIconModule, InputTextModule,
+    ToastModule, ConfirmDialogModule, TableModule],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './list-applications-review-ciarp-members.component.html',
   styleUrl: './list-applications-review-ciarp-members.component.css'
 })
 export class ListApplicationsReviewCiarpMembersComponent {
+
+  @ViewChild('dt1') dt1!: Table;
+
+  first = 0;
+  rows = 5;
+
+  applications: Partial<Application>[] = [];
+
+  applicationTypes: KeyValueOption[] = [
+    { key: 1, value: 'Base salarial - PM-FO-4-FOR-4' },
+    { key: 2, value: 'Bonificación - PM-FO-4-FOR-3' },
+  ];
+
+  applicationStatus: ApplicationStatuses = ApplicationStatuses.SEND_TO_CIARP;
+
+  private readonly confirmationService = inject(ConfirmationService);
+  private readonly router = inject(Router);
+  private readonly messageService = inject(MessageService);
+  private readonly authService = inject(AuthService);
+  private readonly reviewApplicationsManagementUseCase = inject(ReviewApplicationsManagementUseCase);
+  private readonly configurationUseCase = inject(CreateInitialConfigurationUsecase);
+
+  async ngOnInit() {
+
+    await this.getAllApplicationsBySpecificStatus();
+    await this.getAllApplicationsBySpecificStatus(ApplicationStatuses.REVIEWED_BY_CIARP_SECRETARY);
+    await this.getAllApplicationsBySpecificStatus(ApplicationStatuses.REVIEWED_BY_CIARP_MEMBER);
+    await this.getAllApplicationsBySpecificStatus(ApplicationStatuses.ENDORSED_CIARP);
+    await this.getAllApplicationsBySpecificStatus(ApplicationStatuses.REJECTED_CIARP);
+
+  }
+
+  get ApplicationStatus() {
+    return ApplicationStatuses;
+  }
+
+  async getAllApplicationsBySpecificStatus(applicationStatus: ApplicationStatuses = ApplicationStatuses.SEND_TO_CIARP) {
+    return new Promise<void>((resolve, reject) => {
+      this.reviewApplicationsManagementUseCase.getAllApplicationsBySpecificStatus(applicationStatus).subscribe({
+        next: (response: any) => {
+          if (response) {
+            response.forEach((application: any) => {
+              this.applications.push({
+                applicationId: application.applicationId,
+                applicationTypeCatId: application.applicationTypeCatId,
+                applicationTypeName: String(this.applicationTypes.find(x => x.key === application.applicationTypeCatId)?.value || ''),
+                createAt: application.createAt,
+                ciarpSendDate: application.ciarpSendDate,
+                department: application.department,
+                production: application.production,
+                applicationStatus: application.applicationStatus,
+                teacherApplications: application.teacherApplications,
+                nombresSolicitante: application.teacherApplications[0].teacher.person.firstName + ' ' +
+                  application.teacherApplications[0].teacher.person.secondName +
+                  ' ' + application.teacherApplications[0].teacher.person.firstLastName + ' ' +
+                  application.teacherApplications[0].teacher.person.secondLastName,
+              });
+            });
+          }
+          resolve();
+        },
+        error: (error) => {
+          this.messageService.add(
+            {
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Ocurrió un error al obtener las solicitudes'
+            });
+          resolve();
+        }
+
+      });
+    });
+
+  }
+
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  pageChange(event: { first: number; rows: number; }) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
+
+  isLastPage(): boolean {
+    return this.applications ? this.first + this.rows >= this.applications.length : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.applications ? this.first === 0 : true;
+  }
+
+  search(event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    if (target) {
+      this.dt1.filterGlobal(target.value, 'contains');
+    }
+  }
+
+  viewApplication(applicationId: number) {
+
+    this.messageService.add(
+      {
+        severity: 'info',
+        summary: 'Muy pronto estará disponible',
+        detail: 'Esta funcionalidad estará disponible en la siguiente versión.'
+      });
+
+  }
+
+  editApplication(applicationId: number) {
+
+    this.messageService.add(
+      {
+        severity: 'info',
+        summary: 'Muy pronto estará disponible',
+        detail: 'Esta funcionalidad estará disponible en la siguiente versión.'
+      });
+
+  }
+
+  deleteApplication(applicationId: number) {
+
+    this.confirmationService.confirm({
+      target: 'body' as unknown as EventTarget,
+      message: '¿Está seguro(a) de eliminar la solicitud?',
+      header: 'Confirmación',
+      closable: true,
+      closeOnEscape: true,
+      icon: 'pi pi-info-circle',
+      rejectButtonProps: {
+        label: 'Cancelar',
+        severity: 'secondary',
+        outlined: true,
+
+      },
+      acceptButtonProps: {
+        label: 'Aceptar',
+      },
+      accept: async () => {
+        //Llamar al servicio para eliminar la solicitud
+      },
+    });
+
+  }
+
+  viewPdfCertificate(applicationId: number) {
+
+    this.messageService.add(
+      {
+        severity: 'info',
+        summary: 'Muy pronto estará disponible',
+        detail: 'Esta funcionalidad estará disponible en la siguiente versión.'
+      });
+
+  }
+
+  submitApplication(applicationId: number) {
+
+    this.confirmationService.confirm({
+      target: 'body' as unknown as EventTarget,
+      message: '¿Está seguro(a) de remitir la solicitud?',
+      header: 'Confirmación',
+      closable: true,
+      closeOnEscape: true,
+      icon: 'pi pi-info-circle',
+      rejectButtonProps: {
+        label: 'Cancelar',
+        severity: 'secondary',
+        outlined: true,
+
+      },
+      acceptButtonProps: {
+        label: 'Aceptar',
+      },
+      accept: async () => {
+        //Llamar al servicio para enviar la solicitud
+        await this.updateApplicationState(applicationId, ApplicationStatuses.SEND_TO_CIARP);
+        await this.getAllApplicationsBySpecificStatus();
+      },
+    });
+  }
+
+  async updateApplicationState(applicationId: number, applicationStatus: ApplicationStatuses) {
+
+    return new Promise<void>((resolve, reject) => {
+
+      this.reviewApplicationsManagementUseCase.updateApplicationState(applicationId,
+        applicationStatus).subscribe({
+          next: (response: any) => {
+            //Mensaje de exito
+            this.messageService.add({
+              severity: 'success',
+              summary: '¡Solicitud remitida!',
+              detail: 'La solicitud ha sido remitida exitosamente.'
+            });
+            resolve();
+          },
+          error: (error) => {
+            console.error("error", error);
+            //Mensaje de error
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Ups, algo salió mal',
+              detail: 'Tuvimos un problema al actualizar la solicitud de reconocimiento. Inténtelo de nuevo en unos minutos.'
+            });
+            resolve();
+          }
+        });
+
+    });
+  }
 
 }
