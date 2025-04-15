964043c281370bf7745a7b6ed5040cf61212df20
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-04-15 11:36:36 -0500

    feat: se configura url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const ENVIRONMENT = {
     production: true,
-    BASE_URL_API_GSRPI: 'https://api.cloud.gsrpi/gsrpi-api/v1',
+    BASE_URL_API_GSRPI: 'http://10.200.2.52:8080/gsrpi-api/v1',
     BASE_URL_SUPABASE: 'https://bleayfxzpykreishpdvw.supabase.co',
     PUBLIC_API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWF5Znh6cHlrcmVpc2hwZHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4ODYxNDMsImV4cCI6MjA0NzQ2MjE0M30.f2sCGIQPyV0c-7VmAM5MxbwNPe6tAAB_553cNwbODlI',
 };
