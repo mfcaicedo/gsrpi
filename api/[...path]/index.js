058752b2d365cdf8ac1e3bb0e1bbd1ce42704d00
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-04-15 15:19:23 -0500

    feat: ajuste proxi

--- api/[...path]/index.js
+++ api/[...path]/index.js
@@ -1,19 +1,17 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
 export default async function handler(req, res) {
     const pathSegments = req.query.path || [];
-    const path = pathSegments.join('/');
+    const path = Array.isArray(pathSegments) ? pathSegments.join('/') : pathSegments;
 
-    // Reemplaza esto con la URL base de tu API HTTP
     const baseUrl = 'http://10.200.2.52:8080/gsrpi-api/v1';
     const url = `${baseUrl}/${path}`;
-
-    console.log(`Proxying request to: ${url}`);
 
     try {
         const apiRes = await fetch(url, {
             method: req.method,
             headers: {
                 'Content-Type': 'application/json',
-                // Puedes copiar otras cabeceras si es necesario
             },
             body: req.method !== 'GET' && req.method !== 'HEAD' ? JSON.stringify(req.body) : undefined,
         });
