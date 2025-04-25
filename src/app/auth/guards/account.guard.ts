d956e3671532ba720ca3ce0151061595a15ef249
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-04-24 23:33:39 -0500

    feat: se cambia guard de inicio de sesion

--- src/app/auth/guards/account.guard.ts
+++ src/app/auth/guards/account.guard.ts
@@ -1,11 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { map, take } from 'rxjs';
 
 export const accountGuard: CanActivateFn = (route, state) => {
-  if (inject(AuthService).isAuthenticated()) {
-    inject(Router).navigate(['/']);
-    return false;
-  }
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.getSession().pipe(
+    take(1),
+    map(session => {
+      if (session !== null) {
+        router.navigate(['/']);
+        return false;
+      }
+      return true;
+    })
+  );
 };
