8e7864922357087aa1c518cd3d55faa172779d60
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-04-25 00:54:38 -0500

    feat: se agrega cards de opciones en el inicio

--- src/app/shared/components/welcome/welcome.component.ts
+++ src/app/shared/components/welcome/welcome.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../../auth/auth.service';
+import { RoleNames } from '../../../auth/enums/roles.enum';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterModule, CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+
+  role = '';
+
+  private readonly authService = inject(AuthService);
+
+  get RoleNames() {
+    return RoleNames;
+  }
+
+  ngOnInit(): void {
+
+    this.authService.getUserDataSession().subscribe((data: any) => {
+      const roles = data.userRoles;
+      this.role = roles[0].role.name;
+    });
+
+  }
 
 }
