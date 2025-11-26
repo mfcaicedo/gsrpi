6497310fdab62f2e784baccbd9e7718a2ea573fe
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-11-26 16:04:34 -0500

    fix: se ajusta la conexion a la base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,14 @@ export const ENVIRONMENT = {
     production: true,
     // BASE_URL_API_GSRPI: 'http://10.200.2.52:8080/gsrpi-api/v1',
     BASE_URL_API_GSRPI: 'https://gsrpi-api.onrender.com/gsrpi-api/v1',
-    BASE_URL_SUPABASE: 'https://bleayfxzpykreishpdvw.supabase.co',
-    PUBLIC_API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWF5Znh6cHlrcmVpc2hwZHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4ODYxNDMsImV4cCI6MjA0NzQ2MjE0M30.f2sCGIQPyV0c-7VmAM5MxbwNPe6tAAB_553cNwbODlI',
+    BASE_URL_SUPABASE: 'https://epouvlqgmorsqgzddwtn.supabase.co',
+    PUBLIC_API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwb3V2bHFnbW9yc3FnemRkd3RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzE0MjAsImV4cCI6MjA3OTc0NzQyMH0.OkjwAuOWXayku0D6bbNblLKvl135PqAJ9UGl_wD6bJ8',
 };
+
+// 1. Realizar un despliegue en los servidores de UNICAUCA. 
+// 2. Realizar una integración con servicios de las TIC (autenticación, migración de docentes, 
+//     notificaciones, archivos) y posterior adecuar la API para la interoperabilidad con estos servicios.
+// )
+// 3. Pruebas con el usuario en en CPD de la Fiet (refactor de proyecto) --> no seran muchos 
+// 4. El sistema permita hacer solicitudes de más tipos de producción (actualmente solo está revistas). 
+// 5. CIARP -> Verificación de lo actual y realizar todo el proceso para las solicitudes en el CIARP 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const ENVIRONMENT = {
     production: false,
     // BASE_URL_API_GSRPI: 'https://gsrpi-api.onrender.com/gsrpi-api/v1',
     BASE_URL_API_GSRPI: 'http://localhost:8080/gsrpi-api/v1',
-    BASE_URL_SUPABASE: 'https://bleayfxzpykreishpdvw.supabase.co',
-    PUBLIC_API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWF5Znh6cHlrcmVpc2hwZHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4ODYxNDMsImV4cCI6MjA0NzQ2MjE0M30.f2sCGIQPyV0c-7VmAM5MxbwNPe6tAAB_553cNwbODlI',
+    BASE_URL_SUPABASE: 'https://epouvlqgmorsqgzddwtn.supabase.co',
+    PUBLIC_API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwb3V2bHFnbW9yc3FnemRkd3RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzE0MjAsImV4cCI6MjA3OTc0NzQyMH0.OkjwAuOWXayku0D6bbNblLKvl135PqAJ9UGl_wD6bJ8',
 };
